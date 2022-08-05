4b14e4fd339017fc7996c112a06aaac5f9529746
jcruzem <109934669+jcruzem@users.noreply.github.com>
2022-08-05 08:46:37 +0800

    Add files via upload

---
+++ server/djangoapp/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+
+# CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
+
+# CarModelAdmin class
+
+
+# CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+
+
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel)
+
---
+++ server/djangoapp/env.py
@@ -0,0 +1,3 @@
+
+
+ANALYZE_PARAMS = {"api_key": "CmH9EqthHIUkTNWN9eVHPKwhDv5NKqFhpb0H30Jt65ba", "url": "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/0a973fb1-606e-48c0-9c35-1d55168fea64/v1/analyze?version=2019-07-12"}
